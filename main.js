5613c0c2547a8fb7b124bba05d2fb834cd8d92dd
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-04-08 17:07:35 +0100

    added example kata task

--- main.js
+++ main.js
@@ -3,3 +3,14 @@
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function playlistStats(playlist) {
+    const numSongs = playlist.length;
+    let totalLength = 0;
+    for (let i = 0; i < playlist.length; i++) {
+      totalLength += playlist[i];
+    }
+    const minutes = Math.floor(totalLength / 60);
+    const seconds = totalLength % 60;
+  
+    return {numSongs, `${minutes}:${seconds.toString().padStart(2, '0')}`};
+};
--- main.test.js
+++ main.test.js
@@ -1,1 +1,31 @@
 //👉 Write your tests below here:
+import { playlistStats } from './main.js';
+    describe('playlistStats', () => {
+        test('calculates playlist stats correctly', () => {
+          const playlist = [180, 240, 300];
+          const expected = { numSongs: 3, length: "12:00" };
+          const result = playlistStats(playlist);
+          expect(result).toEqual(expected);
+        });
+      
+        test('handles empty playlist', () => {
+          const playlist = [];
+          const expected = { numSongs: 0, length: "0:00" };
+          const result = playlistStats(playlist);
+          expect(result).toEqual(expected);
+        });
+      
+        test('handles single-song playlist', () => {
+          const playlist = [180];
+          const expected = { numSongs: 1, length: "3:00" };
+          const result = playlistStats(playlist);
+          expect(result).toEqual(expected);
+        });
+      
+        test('handles playlist with multiple songs less than one minute in length', () => {
+          const playlist = [30, 45, 20];
+          const expected = { numSongs: 3, length: "1:35" };
+          const result = playlistStats(playlist);
+          expect(result).toEqual(expected);
+        });
+      });
